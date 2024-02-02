2318efc8ff3872a546d1f43a599c9cbbadf81070
lautaguardati <lautaguardati@gmail.com>
2024-02-02 17:42:06 -0300

    Mejoramiento de validación de formulario
    Validar el formulario aplica un borde rojo a cada uno de los campos que tiran error. También muestra los errores en la página y la cantidad de errores en la consola.
    En pruebas.js agregado de más prueba de validación, tanto para errores como para evitar falsos negativos

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@ function validarNombre(nombre) {
     if (nombre.length >= 50) {
         return "Este campo debe tener menos de 50 caracteres";
     }
+    if (!/^[a-z]+$/i.test(nombre)) {
+        return "El campo nombre solo acepta letras";
+    }
     return ""
 }
 
@@ -23,7 +26,55 @@ function validarCiudad(ciudad) {
 
 function validarDescripcionRegalo(descripcionRegalo) {
     if (descripcionRegalo.length === 0) {
-        return "Debe escribir lo que quiere recibir."
+        return "El campo de descripción no puede estar vacío"
+    }
+    if (descripcionRegalo.length >= 100) {
+        return "El campo de descripción no puede tener más de 100 caracteres"
+    }
+    if (!/^[A-z0-9 ]+$/.test(descripcionRegalo)) {
+        return "El campo de descripción solo puede tener números y letras"
     }
     return ""
+}
+
+
+
+$form.onsubmit = validarFormulario;
+function validarFormulario(event) {
+    const nombre = document.formulario.nombre.value;
+    const ciudad = document.formulario.ciudad.value;
+    const descripcionRegalo = document.formulario["descripcion-regalo"].value
+
+    const errorNombre = validarNombre(nombre);
+    const errorCiudad = validarCiudad(ciudad);
+    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+
+    const errores = {
+        nombre: errorNombre,
+        ciudad: errorCiudad,
+        "descripcion-regalo": errorDescripcionRegalo,
+    }
+
+    console.log(manejarErrores(errores));
+    event.preventDefault();
+}
+
+function manejarErrores(errores) {
+    const keys = Object.keys(errores);
+    const $errores = document.querySelector("#errores")
+    let cantidadErrores = 0;
+    keys.forEach(function (key) {
+        const error = errores[key];
+        if (error) {
+            cantidadErrores++;
+            $form[key].className = "error";
+
+            const $error = document.createElement("li");
+            $error.innerText = error;
+            $errores.appendChild($error);
+        } else {
+            $form[key].className = "";
+        }
+    })
+    return cantidadErrores
 }
--- js/pruebas.js
+++ js/pruebas.js
@@ -10,6 +10,16 @@ function probarValidarNombre() {
         'Este campo debe tener menos de 50 caracteres',
         'Validar nombre no validó que el nombre sea menor a 50 caracteres'
     );
+
+    console.assert(
+        validarNombre("Alpha125") === "El campo nombre solo acepta letras",
+        "Validar nombre no validó que el nombre solo tenga letras."
+    )
+
+    console.assert(
+        validarNombre("Lautaro") === "",
+        "Validar nombre no validó que el nombre colocado era válido"
+    )
 }
 
 probarValidarNombre();
@@ -21,18 +31,35 @@ function probarValidarCiudad() {
         validarCiudad("") === "Debe seleccionar la ciudad donde vive",
         "Validar ciudad no validó que el campo de 'ciudad' no está vacío."
     )
+    console.assert(
+        validarCiudad("Santa Fe") === "",
+        "Validar ciudad no funcionó con un nombre de ciudad válido."
+    )
 }
 
 probarValidarCiudad()
 
 
 
-function probarValidarDescripcionRegalo (){
+function probarValidarDescripcionRegalo() {
     console.assert(
-        validarDescripcionRegalo("") === "Debe escribir lo que quiere recibir.",
+        validarDescripcionRegalo("") === "El campo de descripción no puede estar vacío",
         "Validar la descripción del regalo no validó que el campo de los regalos no esté vacío."
     )
-}
+    console.assert(
+        validarDescripcionRegalo("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii") ===
+        "El campo de descripción no puede tener más de 100 caracteres",
+        "Validar descripción del regalo no validó que el campo tenga menos de 100 caracteres"
+    )
+    console.assert(
+        validarDescripcionRegalo("`+`+p`´ñ`12312") === "El campo de descripción solo puede tener números y letras",
+        "Validar descripción regalo no validó que la descripción sea solo números y letras"
+    )
 
+    console.assert(
+        validarDescripcionRegalo("Quiero una play") === "",
+        "Validar la descripción del regalo no validó una descripción válida"
+    )
+}
 
 probarValidarDescripcionRegalo();
